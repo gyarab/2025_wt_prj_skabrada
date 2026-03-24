55584957c76cabda1070d6655892750fae669827
Tris4gone <simon.skabrada.s@gyarab.cz>
2026-03-24 18:59:22 +0100

    new model: color

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Palete
+from .models import Palete, Color
 
 @admin.register(Palete)
 class PaleteAdmin(admin.ModelAdmin):
     list_display = ("name", "number_of_colors", "favourited", "viewed")
     list_filter = ("viewed", "favourited")
     search_fields = ("name",)
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ("name", "hex_id")
+    filter = ("colors")
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+    hex_id = models.CharField(max_length=7)
+    info_misc = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.hex_id})"
+
 class Palete(models.Model):
     name = models.CharField(max_length=255)
-    number_of_colors = models.PositiveSmallIntegerField(blank=True, null=True)  # snake_case
+    number_of_colors = models.PositiveSmallIntegerField(blank=True, null=True)
     added_to_system = models.DateTimeField(auto_now_add=True)
     added_to_favourites = models.DateTimeField(auto_now_add=True)
     favourited = models.DateTimeField(auto_now_add=True)
     viewed = models.BooleanField(default=False)
+
+    colors = models.ManyToManyField(Color, blank=True) 
 
     def __str__(self):
         return self.name
