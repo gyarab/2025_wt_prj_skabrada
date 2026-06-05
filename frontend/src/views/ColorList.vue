9b463fca3f3f4ea24c314e01a0863885f19e5bc9
Šimon Škabrada <simon.skabrada.s@gyarab.cz>
2026-06-05 10:20:13 +0200

    idk

--- frontend/src/views/ColorList.vue
+++ frontend/src/views/ColorList.vue
@@ -1,3 +1,21 @@
 <template>
     <h2>Color List</h2>
 </template>
+
+<script setup>
+
+
+    async function load() {
+        const url = new URL('/api/Color', window.location.origin)
+        const res = await fetch(url)
+        if (!res.ok) throw new Error(`HTTP ${res. status}`)
+        const data = await
+
+    }
+
+
+</script>
+
+<template>
+    
+</template>
